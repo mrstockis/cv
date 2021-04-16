d122bbdf4278f7d84202390d3ddbe0a2de280ccb
Mikael Malmberg <mickem90@gmail.com>
2021-04-16 20:03:48 +1200

    Attempt draw on phone

--- canvas.js
+++ canvas.js
@@ -207,8 +207,8 @@ function paintEnd(event)
         left: canvas.getBoundingClientRect().left,
         top: canvas.getBoundingClientRect().top
     }
-    mouse.x = event.x[0] - canvasPos.left;
-    mouse.y = event.y[0] - canvasPos.top;
+    mouse.x = event[0].x - canvasPos.left;
+    mouse.y = event[0].y - canvasPos.top;
 
     c.stroke();
 
