461839e4c0bbf0b2b400279809bae2ea75eb5bf9
mrstockis <mickem90@gmail.com>
2023-04-05 17:04:33 +1200

    Some styling

--- contact.js
+++ contact.js
@@ -5,12 +5,11 @@ function CreateContactInfo(head=false)
   content = `
     ${ head ? "<h3> Contact</h3>" : "" }
     <p>
-      <span class="pop">022 465 0558</span> &nbsp; <span class="pop">mickem90@gmail.com</span><br>
-      <em><span class="pop">124c Tennyson St., Christchurch 8023</span></em>
+      <span class="pop">+64 22 465 0558</span> &nbsp; <span class="pop">mickem90@gmail.com</span><br>
+
     </p>
   `
-    
-    
+    // <em><span class="pop">18 Blue Jean Ave., Rolleston 7614</span></em>
     document.getElementById('contact-info-container').innerHTML = content;
 
 }
